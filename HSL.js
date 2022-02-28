d0ebcefcdfecd4291c825f6c1d8382bfb858b34e
JohnWick1K47 <69172674+JohnWick1K47@users.noreply.github.com>
2022-03-01 04:55:38 +0530

    Update HSL.js

--- HSL.js
+++ HSL.js
@@ -1,27 +1,6 @@
 
-    document.addEventListener("DOMContentLoaded", function(event) {
-        var player = new Plyr(document.getElementById('player'), {
-            //debug: true
-        });
-            player.on('ready', function(event){
-                var instance = event.detail.plyr;
-
-                var hslSource = null;
-                var sources = instance.media.querySelectorAll('source'), i;
-                for (i = 0; i < sources.length; ++i) {
-                    if(sources[i].src.indexOf('.m3u8') > -1){
-                        hslSource = sources[i].src;
-                    }
-                }
-
-                if (hslSource !== null && Hls.isSupported()) {
-                    var hls = new Hls();
-                        hls.loadSource(hslSource);
-                        hls.attachMedia(instance.media);
-                        hls.on(Hls.Events.MANIFEST_PARSED,function() {
-                            console.log('MANIFEST_PARSED');
-                        });
-                }
-            });
-    });
-
+    window.adshobject = {mass: true, except:["shrink-service.it", "adshrink.it", "adshnk.com", "facebook.com", "Hinditvshow24.blogspot.com"], options:{type:false}};
+    (function(i,s,o,g,r,a,m){i['AdshrinkAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;a.setAttribute('user','ODY4OTg=');a.id=r;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://adshrink.it/adshytics.js?c=' + (Math.random() * 9999),'adshytics');
